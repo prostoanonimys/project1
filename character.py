55ff5bf5de7beec445b4f315c4ed22d7b394c356
anonimnoo <nnlliiaa1@gmail.com>
2024-10-21 17:37:28 +0200

    +mobs,+start,+character

--- character.py
+++ character.py
@@ -1,8 +1,19 @@
 character_characteristic= {
+    "Name": None,
     "Health" :15,
     "Damage" :5,
     "Protection":2,
-    "Gold":0
+    "Gold":0,
+    "Level":0
     }
+
+def set_name(character_name):
+    character_characteristic["Name"] = character_name
+
+character_equipment = {
+    "Weapon": None,
+    "Armor": None,
+    "Accessory": None,
+}
 
 inventory = []
--- start.py
+++ start.py
@@ -1,4 +1,13 @@
-import character
+import random
+from character import *
+from mobs import *
+enemy=slime
+decor="---------------------------------------------------"
 
-print(character.character_characteristic)
-print(character.inventory)
+character_name = input("Введи свое имя")
+set_name(character_name)
+print("Вы открываете дверь в подземелье и спускаетесь по ступенькам в темноту")
+print(decor)
+print("Первый этаж")
+if enemy == slime:
+    print(mobs_text["Slime"])
