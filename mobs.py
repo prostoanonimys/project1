2654458544adea13d1f62cde734ee82f1348066d
anonimnoo <nnlliiaa1@gmail.com>
2024-10-22 09:12:49 +0200

    +enemyaction, +characteraction

--- mobs.py
+++ mobs.py
@@ -1,15 +1,34 @@
+import random
+from character import *
 Slime = {
     "Health": 10,
     "Damage": 1,
-    "Protection": 1
+    "Protection": 1,
+    "Shiel":1
 }
 
 lvl1_mobs = [Slime,]
+
+def enemyaction():
+    emaction = random.randint(1,2)
+    if emaction == 1:
+        print("Враг атакует!")
+        character_characteristic["Health"]-=Slime["Damage"]
+        print(f"Вы получили: {Slime["Damage"]} урона")
+        print(f"Ваше здоровье: {character_characteristic["Health"]}")
+    if emaction ==2:
+        print("Враг защищается!")
+        Slime["Protection"]+=Slime["Shiel"]
+        print(f"{Slime["Protection"]}")
 
 def enemydef(enemy_name):
     if enemy_name == "Slime":
         enemy = Slime
         return enemy
+
+def enemytext(enemy_name):
+    if enemy_name == "Slime":
+        print(mobs_text["Slime"])
 
 mobs_text = {
     "Slime": "Вы слышите отдаленное похлюпывание. И чувствуете что ваши ноги прилепают к полу",
--- start.py
+++ start.py
@@ -4,13 +4,14 @@ from mobs import *
 
 enemy_name="Slime"
 enemy = enemydef(enemy_name)
+enemyaction()
+# set_name()
+
+# print("Вы открываете дверь в подземелье и спускаетесь по ступенькам в темноту")
+# print(decor)
+# print("Первый этаж")
+
+# enemytext(enemy_name)
+
+player_action(enemy)
 print(enemy)
-
-#character_name = input("Введи свое имя")
-#set_name(character_name)
-#print("Вы открываете дверь в подземелье и спускаетесь по ступенькам в темноту")
-#print(decor)
-#print("Первый этаж")
-
-if enemy_name == Slime:
-    print(mobs_text["Slime"])
